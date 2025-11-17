93f9f2f4df093ef1382d553e908e944153a4c69d
rosagogodile <luciagogodile@gmail.com>
2025-11-17 02:14:09 -0500

    started working on random walk and implementing a template class for a pair of values

--- levelgeneration/levelfloor.cpp
+++ levelgeneration/levelfloor.cpp
@@ -1,6 +1,6 @@
 /* Author:          Rosa Knowles
  * Created:         11/16/2025
- * Last Updated:    11/16/2025
+ * Last Updated:    11/17/2025
  * Description:     File that contains implementations for the class `LevelFloor` defined in `levelgeneration.h`
  */
 
@@ -13,3 +13,55 @@
  */
 LevelFloor::LevelFloor(std::mt19937 & rng_ref, uint8_t floor_number): rng(rng_ref), floor_num(floor_number) 
 {}
+
+
+/* Function that returns a matrix from the random walk algorithm
+ * https://www.geeksforgeeks.org/python/random-walk-implementation-python/
+ * Used to generate the skeleton of the dungeon map, some other rooms and stuff will be added on after this
+ */
+MATRIX<uint8_t> LevelFloor::Random_Walk()
+{
+    using namespace std;
+
+    // get num of "skeleton" rooms in the floor (as in, rooms that make up the "skeleton" of the floor)
+    uniform_int_distribution<> size_distr(floor_num, (floor_num * floor_num / 2) + floor_num);
+    // pick a size between `MIN_FLOOR_SIZE` and `MAX_FLOOR_SIZE` based on `size_distr`
+    uint16_t num_skel_rooms = min(max(MIN_FLOOR_SIZE, size_distr(rng)), MAX_FLOOR_SIZE);
+
+    // initialize the random number generation stuff 
+    // in the defines in `levelgeneration.h`, `NORTH` has the smallest value and `EAST` has the largest value
+    uniform_int_distribution<> direction_distr(NORTH, EAST);
+
+    // vector of coordinates that have been marked by the random walker
+    vector<Pair<int16_t>> marked_coordinates;
+
+    Pair<int16_t> current_position(0,0);
+    marked_coordinates.push_back(current_position);
+
+    for (uint16_t i = 0; i < num_skel_rooms; ++i)
+    {
+        // get specified direction to move in
+        uint8_t direction = direction_distr(rng);
+
+        // move in the specified direction
+        switch(direction)
+        {
+            case NORTH:
+                current_position.x2 += 1;
+                break;
+            case SOUTH:
+                current_position.x2 -= 1;
+                break;
+            case WEST:
+                current_position.x1 += 1;
+                break;
+            case EAST:
+                current_position.x1 -= 1;
+                break;
+        }
+
+        marked_coordinates.push_back(current_position);
+    }
+
+    // construct minimap matrix
+}
--- levelgeneration/levelgeneration.h
+++ levelgeneration/levelgeneration.h
@@ -1,6 +1,6 @@
 /* Author:          Rosa Knowles
  * Created:         11/16/2025
- * Last Updated:    11/16/2025
+ * Last Updated:    11/17/2025
  * Description:     Header file for level generation function and class definitions
  */
 
@@ -11,20 +11,79 @@
 #include <cstdint>
 #include <memory>
 #include <random>
+#include <vector>
+#include <algorithm>
 
 #define STANDARD_ROOM_SIZE 50
+
+#define MIN_FLOOR_SIZE 8
+#define MAX_FLOOR_SIZE 30
+
+// directional defines used in the implementation of random walk
+#define NORTH 1
+#define SOUTH 2
+#define WEST  3
+#define EAST  4
 
 // type that stores the id for a tile
 // setup this way so i can easily change it later if needed
 typedef uint8_t TILE_ID;
 
+// type that allows me to define a smart pointer for a dynamically allocated matrix
+// without wanting to blow my fucking brains out 
+template <typename T> using MATRIX = std::unique_ptr<std::unique_ptr<T[]>[]>;
+
+
+
+/* Incredibly basic template class that stores a pair of values
+ * Everything is public here because encapsulation should be unnecessary 
+ */
+template <typename T>
+class Pair
+{
+    public:
+        T x1;
+        T x2;
+
+        Pair(){}
+        Pair(T a1, T a2): x1(a1), x2(a2) {}
+
+        // addition operator overloads: https://en.cppreference.com/w/cpp/language/operators.html
+        Pair<T> & operator+=(const Pair<T> & rhs)
+        {
+            this.x1 += rhs.x1;
+            this.x2 += rhs.x2;
+
+            return *this;
+        }
+        friend Pair<T> operator+(Pair<T> lhs, const Pair<T> & rhs)
+        {
+            lhs += rhs;
+            return lhs;
+        }
+};
+
+/* Function that determines which of two pairs are further to the left
+ * returns true if `lhs` is further left than `rhs`
+ */
+template <typename T>
+bool further_left(const Pair<T> & lhs, const Pair<T> & rhs)
+{
+    return lhs.x1 <= rhs.x1 && lhs.x2 <= rhs.x2; 
+}
+
+
+
+/* Class that stores room information
+ * Contains tile data, as well as references to the connecting rooms
+ */
 class LevelRoom
 {
     private:
         uint8_t num_connections;
         uint8_t empty_conections;
 
-        std::unique_ptr<std::unique_ptr<TILE_ID[]>[]> tile_data;
+        MATRIX<TILE_ID> tile_data;
 
         std::shared_ptr<LevelRoom> north;
         std::shared_ptr<LevelRoom> south;
@@ -35,6 +94,10 @@ class LevelRoom
         LevelRoom();
 };
 
+
+/* Class that stores floor information
+ * Essentially a graph of all rooms in a floor
+ */
 class LevelFloor
 {
     private:
@@ -42,6 +105,8 @@ class LevelFloor
         // reference to a random number generator that will be used to generate the floor
         std::mt19937 & rng;
         uint8_t floor_num;
+
+        MATRIX<uint8_t> Random_Walk();
 
     public:
         LevelFloor(std::mt19937 & rng_ref, uint8_t floor_number);
